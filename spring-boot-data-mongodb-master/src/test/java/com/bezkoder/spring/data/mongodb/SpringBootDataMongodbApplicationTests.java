4e527d4ca9ca884d9c791ff1ea5508516402bc27
ajaikc <kcajay72@gmail.com>
2021-10-06 00:33:39 +0530

    deploy my react app to github pages

---
+++ spring-boot-data-mongodb-master/src/test/java/com/bezkoder/spring/data/mongodb/SpringBootDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.data.mongodb;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
